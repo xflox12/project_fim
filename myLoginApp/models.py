a0295c5ad35062046d14e4cb1a7ca47fb19368f0
Iris <iris.haemmerle@gmail.com>
2020-11-06 10:05:31 +0100

    added User Class

--- myLoginApp/models.py
+++ myLoginApp/models.py
@@ -1,7 +1,10 @@
 import django.db
 
 # Create your models here.
-class User(models.Model):
+from django.forms import models
+
+
+class User2(models.Model):
     user_id = models.AutoField
     name = models.TextField()
     surname = models.TextField()
@@ -13,4 +16,72 @@ class User(models.Model):
 class Favourites(models.Model):
     receipe_id = models.TextField()
     user_id = models.TextField()
+
+
+# Create your models here.
+class User(models.Model):
+    ACTIVE = 'ACT'
+    NEW = 'NEW'
+    LOCKED = 'LOC'
+    DELETED = 'DEL'
+    LIST_OF_VALUES_STATUS = [
+        (ACTIVE, 'Active'),
+        (NEW, 'New'),
+        (LOCKED, 'Locked'),
+        (DELETED, 'Deleted')
+    ]
+
+    # PrimaryKey
+    user_id = models.BigAutoField(primary_key=True
+    , verbose_name="Unique UserId"
+    )
+
+    # First Name with max 100 Characters
+    firstname = models.CharField(verbose_name="Firstname"
+    , max_length=100
+     )
+
+    # Last Name with max 100 Characters
+    surname = models.CharField(verbose_name="Surname"
+    , max_length=100
+     )
+
+    #    @property
+    #    def full_name(self):
+    #        "Returns the person's full name."
+    #        return '%s %s' % (self.firstname, self.surname)
+
+
+    # nickname is used to show on recipes and comments/ratings
+    # not unique
+    nickname = models.CharField(verbose_name="Nickname"
+    , max_length=100
+     )
+
+    # Unique user identification - used for login
+    email = models.EmailField(verbose_name="EMail-Address"
+    , max_length=300
+     )
+
+    # picture is shown on recipes and comments/ratings (maybe it is necessary to add a very small size of the picture)
+    picture = models.ImageField(verbose_name="Userpicture"
+     )
+
+    # ACT ... Activ
+    # NEW ... New - after registration an before confirmation (link sent via email)
+    # LOC ... Locked after 3 login attempts
+    # DEL ... Deleted - not used anymore (delete after X weeks of inactivity)
+    status = models.CharField(verbose_name="Status"
+    , max_length=3
+    , choices=LIST_OF_VALUES_STATUS
+    , default=NEW
+     )
+
+    # time of the last valid login of the user
+    lastlogin = models.DateTimeField(verbose_name="Last Login Date"
+     )
+
+    # how often has the user tried to login (maybe with wrong password) - after 3 attempts the user-status is changed to LOC
+    retrylogin = models.PositiveSmallIntegerField(verbose_name="Number of Login Attempts"
+    )
 
