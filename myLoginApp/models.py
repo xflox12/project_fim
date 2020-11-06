d18b737f285552e3e2dbae369372e1d2052f5b11
Iris <iris.haemmerle@gmail.com>
2020-11-06 16:15:57 +0100

    changes model

--- myLoginApp/models.py
+++ myLoginApp/models.py
@@ -1,9 +1,8 @@
-import django.db
+# import django.db
 
 # Create your models here.
 from django.db import models
-#from django.contrib.admin import models
-
+# from django.contrib.admin import models
 
 
 class User2(models.Model):
@@ -17,7 +16,7 @@ class User2(models.Model):
 
 
 class Favourites(models.Model):
-    receipe_id = models.TextField()
+    recipe_id = models.TextField()
     user_id = models.TextField()
 
 
@@ -35,18 +34,18 @@ class User(models.Model):
     ]
 
     # PrimaryKey
-    user_id = models.BigAutoField(primary_key=True
-                                  , verbose_name="Unique UserId"
+    User_ID = models.BigAutoField(primary_key=True,
+                                  verbose_name="Unique UserId"
                                   )
 
     # First Name with max 100 Characters
-    firstname = models.CharField(verbose_name="Firstname"
-                                 , max_length=100
+    Firstname = models.CharField(verbose_name="Firstname",
+                                 max_length=100
                                  )
 
     # Last Name with max 100 Characters
-    surname = models.CharField(verbose_name="Surname"
-                               , max_length=100
+    Surname = models.CharField(verbose_name="Surname",
+                               max_length=100
                                )
 
     #    @property
@@ -56,33 +55,34 @@ class User(models.Model):
 
     # nickname is used to show on recipes and comments/ratings
     # not unique
-    nickname = models.CharField(verbose_name="Nickname"
-                                , max_length=100
+    Nickname = models.CharField(verbose_name="Nickname",
+                                max_length=100
                                 )
 
     # Unique user identification - used for login
-    email = models.EmailField(verbose_name="EMail-Address"
-                              , max_length=300
+    Email = models.EmailField(verbose_name="EMail-Address",
+                              max_length=300
                               )
 
     # picture is shown on recipes and comments/ratings (maybe it is necessary to add a very small size of the picture)
-    picture = models.ImageField(verbose_name="Userpicture"
+    Picture = models.ImageField(verbose_name="UserPicture"
                                 )
 
-    # ACT ... Activ
+    # ACT ... Active
     # NEW ... New - after registration an before confirmation (link sent via email)
     # LOC ... Locked after 3 login attempts
     # DEL ... Deleted - not used anymore (delete after X weeks of inactivity)
-    status = models.CharField(verbose_name="Status"
-                              , max_length=3
-                              , choices=LIST_OF_VALUES_STATUS
-                              , default=NEW
+    Status = models.CharField(verbose_name="Status",
+                              max_length=3,
+                              choices=LIST_OF_VALUES_STATUS,
+                              default=NEW
                               )
 
     # time of the last valid login of the user
-    lastlogin = models.DateTimeField(verbose_name="Last Login Date"
+    LastLogin = models.DateTimeField(verbose_name="Last Login Date"
                                      )
 
-    # how often has the user tried to login (maybe with wrong password) - after 3 attempts the user-status is changed to LOC
-    retrylogin = models.PositiveSmallIntegerField(verbose_name="Number of Login Attempts"
+    # how often has the user tried to login (maybe with wrong password)
+    # after 3 attempts the user-status is changed to LOC
+    RetryLogin = models.PositiveSmallIntegerField(verbose_name="Number of Login Attempts"
                                                   )
