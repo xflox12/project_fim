7fc0c6702b5261d665834d265cc9c8f4157cc355
IrisHaemm <72600894+IrisHaemm@users.noreply.github.com>
2020-11-17 19:30:00 +0100

    Revert "Development login registration"

--- myLoginApp/views.py
+++ myLoginApp/views.py
@@ -1,30 +1,10 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View, TemplateView   #import for class-based-view #import for template-view
 # from django.views.generic import TemplateView #add all in one line
 
-from .forms import create_user_form
-from django.contrib.auth import authenticate,login
-# Create your views here.
-
-def user_registration(httprequest, *args, **kwargs): #define the function for the registration form
-    if httprequest.method == "POST":
-        form = create_user_form(httprequest.POST)
-
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['Email'] #username = email
-            password = form.cleaned_data['Password']
-            user = authenticate(username=username, password=password)
-            login(httprequest, user)
-            return redirect("")
-
-    else:
-        form = create_user_form()
-        context = {
-            "form": form
-        }
-    return render(httprequest, "registration.html", context)
+from .models import User2
 
 # Create your views here.
+
 
