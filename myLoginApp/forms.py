745bcb17d331b36e33b08be0ffe2f304f08640df
marionasero <72811129+marionasero@users.noreply.github.com>
2020-11-25 16:44:48 +0100

    Views
    Changes

--- myLoginApp/forms.py
+++ myLoginApp/forms.py
@@ -4,5 +4,5 @@ from .models import User
 class create_user_form(forms.Form): #form the people will see when registering for the first time
     class Meta: #defining the data
         model=User
-        fields=["Firstname", "Surname", "Nickname", "Email", "Picture", ] #inputs users will need to fill out while registering
+        fields=["Firstname", "Surname", "Nickname", "Email", "Picture", "Owner" ] #inputs users will need to fill out while registering
 
