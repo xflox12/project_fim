ab09a58d2aa4c1b81365f615c8168bcd8009b05e
marionasero <72811129+marionasero@users.noreply.github.com>
2020-11-27 17:00:23 +0100

    Html changes + Registration directory created

--- myLoginApp/views.py
+++ myLoginApp/views.py
@@ -8,7 +8,7 @@ from .forms import create_user_form
 # Create your views here.
 
 
-def user_registration(httprequest, *args, **kwargs): # define the function for the registration form
+def user_registration(httprequest, *args, **kwargs):  # define the function for the registration form
     """this function consists of the creation of a user registration form and, after the user is successfully registered, it automatically logs him/her into their profile"""
     if httprequest.method == "POST":
         form = create_user_form(httprequest.POST)
@@ -19,7 +19,7 @@ def user_registration(httprequest, *args, **kwargs): # define the function for t
             Password = form.cleaned_data['Password']
             user = authenticate(Nickname=Nickname, Password=Password)
             login(httprequest, user)
-            return redirect("") #redirect page needs to be added
+            return redirect("")  # redirect page needs to be added
 
     else:
         form = create_user_form()
