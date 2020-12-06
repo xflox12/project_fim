a2966bd31d2d0c8228e335c4f8f5781f5d1e5012
Iris <iris.haemmerle@gmail.com>
2020-12-06 10:47:28 +0100

    changed registration and committing database

--- myLoginApp/models.py
+++ myLoginApp/models.py
@@ -2,9 +2,24 @@
 # from django.contrib.admin import models
 
 from django.contrib.auth.models import User
-
-# from django.db import models
-
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    UserId = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # picture is shown on recipes and comments/ratings (maybe it is necessary to add a very small size of the picture)
+    Picture = models.ImageField(verbose_name="Profile Picture",
+                                blank=True
+                                )
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
