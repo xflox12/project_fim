2914bed58756ff1e974cdba839fb6ec13f8ed41e
Florian Schietinger <schiflo5991@gmail.com>
2020-12-12 00:03:46 +0100

    Profil is only visible when logged in

--- myProfileApp/views.py
+++ myProfileApp/views.py
@@ -3,18 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def profile_view_temp(httprequest, *args, **kwargs):             #view with template
+def profile_view_temp(request, *args, **kwargs):             #view with template
     """ my_dict = {
         "name": "Flo",
         "lastname": "Schietinger",
         "myList": ['this', 'is', 'my', 'list']
-
-
     }"""
+    user= None
+    if user.is_authenticated():
+        user=request.user
 
-    user = {
-        "UserId": "23",
-        "firstname": "Yasmin",
-        "lastname": "Jung",
-            }
-    return render(httprequest, "myprofile.html", user)
+    return render(request, "myprofile.html", user)
