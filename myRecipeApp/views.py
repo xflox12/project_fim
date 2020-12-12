db57af6f1d8b5903f6f3cd2738091449ed3c565a
marionasero <72811129+marionasero@users.noreply.github.com>
2020-12-12 19:17:02 +0100

    Add comments functionality started
    still working on it
    (couldn't migrate due to some errors appearing coming from another functionality)

--- myRecipeApp/views.py
+++ myRecipeApp/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse  # For Function-Based-Views
 from django.views.generic import View, TemplateView, ListView
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
 from django.db import IntegrityError
 # from .forms import UnitForm
 from .models import Recipe, RecipeSteps, Ingredient, Category, RecipeCategory
-from .forms import create_recipe_form, create_recipe_form2, create_recipe_form3
+from .forms import create_recipe_form, create_recipe_form2, create_recipe_form3, CommentForm
 from django import forms
 
 #import Models
@@ -98,6 +98,30 @@ def add_step(request,recipe_id) :
     }
 
     return render(request, template, context)
+
+def add_comments(request, slug):
+    template_name = 'dev_add_comments.html'
+    post = get_object_or_404(Recipe, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, template_name, {'post': post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
 
 # Create your views here.
 def recipes_list_view_temp(httprequest, my_id, *args, **kwargs):             #view with template
